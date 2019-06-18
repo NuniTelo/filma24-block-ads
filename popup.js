970a314021af650be94ac49040bdcd085ebcf73f
Naun Telo <naun.telo@pradiumtech.com>
2019-06-18 15:47:15 +0200

    first

---
+++ popup.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+
+    //change to stopautoplay first 
+    let iframe_video =  document.getElementById("bastplayer")
+    iframe_video.parentNode.removeChild(iframe_video)
+    
+    let vid_add = document.querySelector("#vidad")
+    vid_add.parentNode.removeChild(vid_add)
+}
+
+
